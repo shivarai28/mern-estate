7442ba134c3a4c26a1f4b3b90c64dc4fbe9a0469
shivarai28 <shivarai9098@gmail.com>
2025-09-23 19:08:24 +0530

    create registration form component with username, email, and password fields

--- client/src/pages/Register.jsx
+++ client/src/pages/Register.jsx
@@ -1,7 +1,23 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 export default function Register() {
   return (
-    <div>Register</div>
+        <div className='max-w-lg p-3  mx-auto '>
+
+   
+
+      <h1  className='text-3xl font-semi-bold text-center p-2 my-7'>Sign Up</h1>
+      <form className='flex flex-col gap-4 my-5  ' >
+        <input type="text" placeholder='Username'  className='outline-none  bg-slate-200 p-3 rounded-lg  focus:bg-slate-300 '/>
+        <input type="email" placeholder='Email' className='outline-none p-3 bg-slate-200 rounded-lg  focus:bg-slate-300' />
+        <input type="password" placeholder='Password'  className='outline-none bg-slate-200  p-3 rounded-lg  focus:bg-slate-300'/>
+        <button type='submit' className='bg-slate-700 mt-5 py-2 mx-45 rounded-md hover:bg-slate-950 hover:text-white'>Register</button>
+      </form>
+
+      <p className='mt-5 px-33'>Already have an account?<Link to={'/login'} ><span className='text-blue-600 cursor-pointer'>Login</span></Link></p>
+
+
+    </div>
   )
 }
