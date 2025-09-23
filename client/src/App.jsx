4997e69c8b55c513ead0bb6103bb4a03f3346625
shivarai28 <shivarai9098@gmail.com>
2025-09-23 16:32:20 +0530

    hello

---
+++ client/src/App.jsx
@@ -0,0 +1,36 @@
+
+import './App.css'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import Home from './pages/Home'
+import About from './pages/About'
+import Profile from './pages/Profile'
+import Login from './pages/Login'
+import Register from './pages/Register'
+import NotFound from './pages/NotFound'
+import Header from './components/Header'
+
+
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Header /> 
+    
+      
+    <Routes>
+      <Route path='/' element={<Home/>} />
+      <Route path='/about' element={<About/>} />   
+      <Route path = '/profile' element = {<Profile/>} />
+      <Route path = '/login' element = {<Login/>} />
+      <Route path = '/register' element = {<Register/>} />
+      <Route path = '*' element = {<NotFound/>} />
+   </Routes>
+
+
+</BrowserRouter>
+
+    
+  )
+}
+
+export default App
